12daed4706d02ae0f9408866c47a38c0e23a71d2
GGICE <309683593@qq.com>
2014-12-15 15:52:46 +0800

    optimize.

--- app/uab/uab.js
+++ app/uab/uab.js
@@ -2,9 +2,22 @@
  * Created by ggice on 14/12/13.
  */
 "use strict";
-(function uabase(){
+
+//Build Event Listeners
+function addEventHandler(a, b, c) {
+    if (a.addEventListener) {
+        a.addEventListener(b, c, false)
+    } else if (a.attachEvent) {
+        a.attachEvent("on" + b, c)
+    } else {
+        a["on" + b] = c
+    }
+};
+//the base
+(function uabase() {
     var data = [];
-  //  create a heatmap instance
+    var that = {};
+    //  create a heatmap instance
     var heatmap = h337.create({
         container: document.getElementsByTagName("BODY")[0],
         maxOpacity: .3,
@@ -13,26 +26,21 @@
         // backgroundColor with alpha so you can see through it
         backgroundColor: 'rgba(0, 0, 58, 0.10)'
     });
-    document.onclick = function(e) {
-        var temData;
-        var className = e.target.className,
-            id = e.target.id,
-            href = e.target.href,
-            x = e.layerX,
-            y = e.layerY;
-        temData = {
-            "className" : className,
-            "id" : id,
-            "href" : href,
-            "x" : x,
-            "y" : y
-        };
+    //detection page click
+    that.detectionPageClick = function (e) {
+        var temData = {};
+            temData.className = e.target.className,
+            temData.id = e.target.id,
+            temData.href = e.target.href,
+            temData.x = e.layerX,
+            temData.y = e.layerY;
         console.log(temData);
         data.push(temData);
         console.log(data);
-        heatmap.addData({ x: x, y: y, value: 1 });
+        heatmap.addData({ x: temData.x, y: temData.y, value: 1 });
     };
-    window.onbeforeunload = function() {
+    addEventHandler(document, "click", that.detectionPageClick);
+    window.onbeforeunload = function () {
         var showData = JSON.stringify(data);
         return showData;
     };
